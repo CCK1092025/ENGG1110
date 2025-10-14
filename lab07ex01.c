eb332000051929b293b9b7712b93919d05834c2d
Matthew <chancheukkin930@gmail.com>
2025-10-14 11:15:15 +0800

    Add files via upload

---
+++ lab07ex01.c
@@ -0,0 +1,46 @@
+/**
+ * ENGG1110 Problem Solving by Programming 
+ *
+ * I declare that the assignment here submitted is original
+ * except for source material explicitly acknowledged,
+ * and that the same or closely related material has not been
+ * previously submitted for another course.
+ * I also acknowledge that I am aware of University policy and
+ * regulations on honesty in academic work, and of the disciplinary
+ * guidelines and procedures applicable to breaches of such
+ * policy and regulations, as contained in the website.
+ *
+ * University Guideline on Academic Honesty:
+ *   http://www.cuhk.edu.hk/policy/academichonesty/
+ *
+ * Student Name : Chan Cheuk Kin
+ * Student ID   : 1155257970
+ * Class/Section: ENGG1110B
+ * Date         : 13/10/2025
+ */
+
+#include <stdio.h>
+
+int main(void) {
+  
+
+    int stars = -1;
+    int i=0;
+    
+    while(stars != 0){
+    printf("How many stars do you want? ");
+       scanf("%d", &stars);
+       
+       for(i=0;i<stars;i++){
+          printf("*"); 
+       }
+        if(stars > 0)
+       printf("\n");
+    } 
+      
+    
+    
+
+    
+    return 0;
+}
